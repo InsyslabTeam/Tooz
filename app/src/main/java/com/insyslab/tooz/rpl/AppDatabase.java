b322923ab88c1d1401f1165ee3c13e61d3874152
Tanmay <krishna.t@ezetap.com>
2018-04-07 18:42:23 +0530

    release build

--- app/src/main/java/com/insyslab/tooz/rpl/AppDatabase.java
+++ app/src/main/java/com/insyslab/tooz/rpl/AppDatabase.java
@@ -13,7 +13,7 @@ import com.insyslab.tooz.models.UserGroup;
  * Created by TaNMay on 07/12/17.
  */
 
-@Database(version = 1, entities = {User.class, UserGroup.class,Reminder.class, PhoneContact.class, LocalReminder.class})
+@Database(version = 2, entities = {User.class, UserGroup.class,Reminder.class, PhoneContact.class, LocalReminder.class})
 public abstract class AppDatabase extends RoomDatabase {
 
     public static final String DB_NAME = "tooz_db";
